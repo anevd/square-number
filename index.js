cb6643e753320878ef98b950e6e6026aba5901c9
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-01-14 18:37:27 +0300

    add the calculation of the square of a number

---
+++ index.js
@@ -0,0 +1,11 @@
+const number = document.querySelector("#number");
+const button = document.querySelector(".square__button");
+const result = document.querySelector("#result");
+
+button.addEventListener("click", function () {
+	result.value = "";
+	if (number.validity.valid && number.value != "") {
+		result.value = Math.pow(+number.value, 2);
+		console.log(Math.pow(+number.value, 2));
+	}
+});
